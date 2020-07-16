17f3122fe36fad689aa35e3b67fa8f6ca9086d7a
FaroukAjimi <benajimi@gmail.com>
2020-07-16 20:28:57 +0100

    Soo

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""imprt"""
+"""contcatinate axis"""
 
 
 def np_cat(mat1, mat2, axis=0):
-    """np cat"""
-    return(np.concatenate((mat1,mat2), axis=axis))
+    """np cat axis in dim"""
+    return(np.concatenate((mat1, mat2), axis=axis))
