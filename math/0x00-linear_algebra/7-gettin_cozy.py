0bca2b35e08721a9038ba65a68641682d6b10fca
FaroukAjimi <benajimi@gmail.com>
2020-07-16 22:02:54 +0100

    Soo

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
-"""copet"""
-import copy
+"""copet cat matrice"""
+
+
 def cat_matrices2D(mat1, mat2, axis=0):
-    """ cat mat"""
+    """ cat mat 2d"""
     new = []
     lis1 = []
-    a = copy.deepcopy(mat1)
-    b = copy.deepcopy(mat2)
     try:
         if axis == 0:
-            for i in range(len(a)):
-                new.append(a[i])
-            for i in range(len(b)):
-                new.append(b[i])
+            for i in range(len(mat1)):
+                new.append(mat1[i])
+            for i in range(len(mat2)):
+                new.append(mat2[i])
         elif axis == 1:
-            for i in range(len(a)):
-                new.append(a[i])
-            for i in range(len(b)):
-                for y in range(len(b[i])):
-                    new[i].append(b[i][y])
+            for i in range(len(mat1)):
+                new.append(mat1[i])
+            for i in range(len(mat2)):
+                for y in range(len(mat2[i])):
+                    new[i].append(mat2[i][y])
         return(new)
     except:
         return(None)
