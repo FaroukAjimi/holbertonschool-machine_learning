c1b28333426b50984ee8694b47ca9b354954bfff
FaroukAjimi <benajimi@gmail.com>
2020-07-16 23:13:33 +0100

    Soo

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,21 +5,11 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """ cat mat 2d"""
     new = []
-    lis1 = []
-    if len(mat1) != len(mat2):
-        return(None)
-    for i in range(len(mat1)):
-        if len(mat1[i]) != len(mat2[i]):
-            return(None)
-    if axis == 0:
+    lis = []
+    if axis == 0 and (len(mat1[0]) == len(mat2[0])):
+        return(mat1 + mat2)
+    elif axis == 1 and (len(mat1) == len(mat2)):
         for i in range(len(mat1)):
-            new.append(mat1[i])
-        for i in range(len(mat2)):
-            new.append(mat2[i])
-    elif axis == 1:
-        for i in range(len(mat1)):
-            new.append(mat1[i])
-        for i in range(len(mat2)):
-            for y in range(len(mat2[i])):
-                new[i].append(mat2[i][y])
-    return(new)
+            lis = mat1[i] + mat2[i]
+            new.append(lis)
+        return(new)
