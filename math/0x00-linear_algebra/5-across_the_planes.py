4fadb273081f269f1fa93d52bbf0efe8f2bec3f7
FaroukAjimi <benajimi@gmail.com>
2020-07-16 22:29:09 +0100

    Soo

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -5,13 +5,13 @@
 def add_matrices2D(mat1, mat2):
     """add 2d matrix"""
     new = []
-    if (len(mat1) != len(mat2)):
+    if len(mat1) != len(mat2):
         return(None)
     for i in range(len(mat1)):
         if len(mat1[i]) != len(mat2[i]):
             return None
     for i in range(len(mat2)):
         new.append([])
-        for y in range(len(mat1)):
+        for y in range(len(mat1[i])):
             new[i].append(mat1[i][y] + mat2[i][y])
     return(new)
