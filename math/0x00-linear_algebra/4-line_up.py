bcd7f56231846b709390bf6cf2d2e71f4a2db61c
FaroukAjimi <benajimi@gmail.com>
2020-07-16 19:51:56 +0100

    ()

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -5,8 +5,8 @@
 def add_arrays(arr1, arr2):
     """add"""
     if len(arr1) != len(arr2):
-        return(None)
+        return None
     new = []
     for i in range(len(arr1)):
         new.append(arr1[i] + arr2[i])
-    return(new)
+    return new
