c9d29db5de714d2528513793586a7f234b307128
FaroukAjimi <benajimi@gmail.com>
2020-07-16 18:56:43 +0100

    S

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -5,8 +5,12 @@
 def add_arrays(arr1, arr2):
     """add"""
     new = []
+    if len(arr1) != arr2:
+        return(None)
+    if arr1 == [] or arr2 == []:
+        return(None)
     if len(arr1) != len(arr2):
         return(None)
     for i in range(len(arr1)):
-        new.append(arr1[i]+arr2[i])
+        new.append(arr1[i] + arr2[i])
     return(new)
