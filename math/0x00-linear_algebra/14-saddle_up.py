c20163373128341395b207b0cd1b829c113fc08b
FaroukAjimi <benajimi@gmail.com>
2020-07-16 20:42:08 +0100

    14

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+""" Np matmul mat1 mat2"""
+
+
+import numpy as np
 
 
 def np_matmul(mat1, mat2):
-    """sos"""
-    return(mat1.dot(mat2))
+    """mat mul mat1 mat"""
+    return(np.matmul(mat1, mat2))
