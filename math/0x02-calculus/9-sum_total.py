083753b026f66ecf76eef47aafb37fb4c1d46f77
FaroukAjimi <benajimi@gmail.com>
2020-07-30 11:53:28 +0100

    solid

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def summation_i_squared(n):
-    try:
-        lis = [*range(1, n + 1)]
-    except:
+    t = type(1)
+    if type(n) is not t:
         return(None)
+    lis = [*range(1, n + 1)]
     po = list(map(lambda x: x ** 2, lis))
     return (sum(po))
