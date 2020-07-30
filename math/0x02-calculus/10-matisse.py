c6439dabc82f2ad49a45dc45e5cc8018236de292
FaroukAjimi <benajimi@gmail.com>
2020-07-30 13:35:46 +0100

    Sooo

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -8,10 +8,12 @@ def poly_derivative(poly):
     t1 = type(1)
     if type(poly) is not t:
         return(None)
+    if len(poly) == 0:
+        return(None)
+    if len(poly) == 1:
+        return([0])
     der = []
     for i in range(len(poly)):
         if i != 0:
             der.append(i * poly[i])
-    if der == []:
-        return(0)
     return(der)
