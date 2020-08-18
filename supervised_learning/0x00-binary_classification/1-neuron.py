d9d44686107db81c8135f056cefa909167c4bfba
FaroukAjimi <benajimi@gmail.com>
2020-08-18 12:28:50 +0100

    Deadlione

---
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Supervised leaning intro"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """Class neuron"""
+    def __init__(self, nx):
+        "init func"""
+        if not(isinstance(nx, int)):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = (np.random.randn(1, nx))
+        self.__b = 0
+        self.__A = 0
+        print(dir(self))
+
+    @property
+    def W(self):
+        """W selr"""
+        return (self._Neuron__W)
+
+    @property
+    def b(self):
+        """b soof"""
+        return (self.__b)
+
+    @property
+    def A(self):
+        """yoo"""
+        return (self.__A)
