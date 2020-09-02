8ce36b6b73b9f2113ea8a9821be59ae244ae05f9
FaroukAjimi <benajimi@gmail.com>
2020-09-02 13:53:18 +0100

    So

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+"""slicing like a ninja"""
 
 
 def np_slice(matrix, axes={}):
+    """slice function
+    @matrix: matrix to slice
+    @axes: axes"""
     lis = []
     for i in range(len(matrix.shape)):
         t = slice(*axes.get(i, (None, None)))
