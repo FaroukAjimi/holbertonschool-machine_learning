2d412ab060fd537caba2db9cfbc0d9511db861f5
FaroukAjimi <benajimi@gmail.com>
2020-09-03 03:09:33 +0100

    soo

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -7,10 +7,10 @@ def poly_integral(poly, C=0):
     integral = [C]
     if not(isinstance(poly, list)) or not(isinstance(C, int)):
         return(None)
-    if len(poly) == 0 or poly == None:
+    if len(poly) == 0 or poly is None:
         return(None)
     if poly == [0]:
-        return(C)
+        return([C])
     for i in range(len(poly)):
         if i != 0:
             s = (poly[i] ** i)/(i+1)
